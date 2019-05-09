9c0fc9789b31af3149005a0aaf339c6f181f1a6c
christopherkade <c.kade96@gmail.com>
2019-05-09 07:58:50 +0200

    🔥 Removed unused search component

--- src/components/Article/ArticleSearch/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ArticleSearch } from "./ArticleSearch"
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,12 +3,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import { ArticleList } from "../components/Article/ArticleList"
-import { ArticleSearch } from "../components/Article/ArticleSearch"
 
 const Blog = () => (
   <Layout>
     <SEO title="Blog" keywords={[`javascript`, `blog`, `react`]} />
-    <ArticleSearch />
     <ArticleList />
   </Layout>
 )
